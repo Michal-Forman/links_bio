49859e6697d1161523c704edf2f68e13983125ea
Michal Forman <miska.forman@gmail.com>
2024-03-28 15:56:53 +0100

    first version

---
+++ main.js
@@ -0,0 +1,19 @@
+function calculateMyAge() {
+  const birthdate = "6.3.2007";
+  const [day, month, year] = birthdate
+    .split(".")
+    .map((num) => parseInt(num, 10));
+  const birthDateObj = new Date(year, month - 1, day);
+  const today = new Date();
+  let age = today.getFullYear() - birthDateObj.getFullYear();
+  const m = today.getMonth() - birthDateObj.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birthDateObj.getDate())) {
+    age--;
+  }
+
+  return age;
+}
+
+let ageSpan = document.getElementById("calculated-age");
+ageSpan.textContent = calculateMyAge();
